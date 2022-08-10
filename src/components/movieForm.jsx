2cff50aa73553ff762f150ed75f86a0894788075
Fabio Coradini <fabiosrc@gmail.com>
2022-08-10 17:26:01 -0300

    recover the movie in movie form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,49 @@
-﻿import React, { Component } from "react";
+﻿import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
+import { getMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 
-class MovieDetails extends Component {
-  handleSave = () => {
+class MovieForm extends Form {
+  state = {
+    data: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: "",
+    },
+    errors: {},
+    genres: [],
+  };
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    if (id) {
+      const { title, genre, numberInStock, dailyRentalRate } = getMovie(id);
+      const data = {
+        ...this.state.data,
+        title,
+        genreId: genre._id,
+        numberInStock,
+        dailyRentalRate,
+      };
+      this.setState({ data });
+
+      this.setState({
+        genres: [{ name: "All Genres" }, ...getGenres()],
+      });
+    }
+  }
+
+  schema = {
+    title: Joi.string().required().label("Username"),
+    genre: Joi.string().required().label("Genre"),
+    numberInStock: Joi.string().required().label("Number in stock"),
+    dailyRentalRate: Joi.string().required().label("Rate"),
+  };
+
+  onSubmit = () => {
+    console.log("saving", this.state);
     // Navigate to /products
     this.props.history.push("/movies");
   };
@@ -9,13 +51,17 @@ class MovieDetails extends Component {
   render() {
     return (
       <div>
-        <h1>Movie Details - {this.props.match.params.id} </h1>
-        <button className="btn btn-primary" onClick={this.handleSave}>
-          Save
-        </button>
+        <h2>Movie - {this.props.match.params.id}</h2>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Username")}
+          {/* {this.renderInput("genre", "Genre")} */}
+          {this.renderInput("numberInStock", "Number in stock", "numeric")}
+          {this.renderInput("dailyRentalRate", "Rate", "numeric")}
+          {this.renderButton("Register")}
+        </form>
       </div>
     );
   }
 }
 
-export default MovieDetails;
+export default MovieForm;
